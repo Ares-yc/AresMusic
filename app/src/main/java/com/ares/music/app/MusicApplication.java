b33a76029ebcb0190d4dd5a02f5d97876727270f
颜崔 <237387718@qq.com>
2018-03-28 22:01:54 +0800

    Update

---
+++ app/src/main/java/com/ares/music/app/MusicApplication.java
@@ -0,0 +1,19 @@
+package com.ares.music.app;
+
+import android.app.Application;
+
+/**
+ * ====================================
+ * 作    者：Ares(颜崔)
+ * 地    址：https://github.com/Ares-yc
+ * 描    述：
+ * 版    本：1.0.0
+ * 创建时间：2018/3/28.
+ * 修改时间：2018/3/28.
+ * ====================================
+ */
+
+public class MusicApplication extends Application{
+
+
+}
